bf83718e4df9a20fb27e41dc2d34c08e220bf6d3
Mazen Elmesery <mazenelmesery@gmail.com>
2022-02-14 19:10:36 +0200

    add category model

---
+++ models/category.ts
@@ -0,0 +1,36 @@
+import { Model, Schema, model } from "mongoose";
+const Joi = require("joi");
+
+interface ICategory {
+  name: string;
+}
+
+interface CategoryModel extends Model<ICategory> {
+  lookup(email: string): Promise<any>;
+}
+
+const categorySchema = new Schema<ICategory, CategoryModel>({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+});
+
+categorySchema.static("lookup", function lookup(name: string) {
+  return this.findOne({ name });
+});
+
+export const Category = model<ICategory, CategoryModel>(
+  "Category",
+  categorySchema
+);
+
+export function validateCategory(category: any) {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+  });
+
+  return schema.validate(category);
+}
