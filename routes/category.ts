c904faf5cce7b82830e03011ec974090052471d5
Mazen Elmesery <mazenelmesery@gmail.com>
2022-02-17 20:10:52 +0200

    remove unused import

--- routes/category.ts
+++ routes/category.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import { Request, Response, NextFunction } from "express";
 import { validate } from "../middlewares/validate";
 import { Category, validateCategory } from "../models/category";
 import _ from "lodash";
